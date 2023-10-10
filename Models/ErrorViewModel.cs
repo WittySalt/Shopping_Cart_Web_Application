5e2a80f39d2cbd7250476edd88964a9692a6ce64
WitchSaraha <2579706652@qq.com>
2023-10-10 09:08:07 +0800

    添加项目文件。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shopping_Cart_Web_Application_V1._0.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
