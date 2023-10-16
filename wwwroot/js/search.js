f2792c0bc79f99f8491ccd296920be0d8f84e752
CH4NGEbetter <147386123+CH4NGEbetter@users.noreply.github.com>
2023-10-16 11:28:28 +0800

    Add files via upload

---
+++ wwwroot/js/search.js
@@ -0,0 +1,5 @@
+﻿function search() {
+    var searchString = document.getElementById('searchString').value;
+    window.location.href = `/Cart/Index?searchString=${searchString}`;
+}
+//gain searchString from id
