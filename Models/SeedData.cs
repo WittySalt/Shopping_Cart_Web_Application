6f6bd4f93eb6a7e14c18b2bac12d807aebc112cd
WitchSaraha <2579706652@qq.com>
2023-10-26 12:20:42 +0800

    Final-Version-2.0

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -45,7 +45,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                 context.Product.AddRange(
                     new Product
                     {
-                        Id = 1,
                         ProductName= "Introduction to ASP.NET",   
                         ProductDescription = "Dive into the essentials of ASP.NET, understanding its architecture, versions,and setting up the development environment.Perfect for those starting their journey in web development.",
                         Image = "NET1.png",
@@ -53,7 +52,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new Product
                     {
-                        Id = 2,
                         ProductName = "Web Layouts and Controls with ASP.NET",
                         ProductDescription = "Learn to create intuitive page layouts using Web Forms controls, master the page life cycle, and interact seamlessly with both the UI and server-side code.",
                         Image = "NET2.png",
@@ -61,7 +59,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new Product
                     {
-                        Id = 3,
                         ProductName = "State Management in ASP.NET",
                         ProductDescription = "Explore various techniques for managing state in web applications, from view state to query strings, and learn to implement them in an e-commerce shopping cart scenario.",
                         Image = "NET3.png",
@@ -69,7 +66,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new Product
                     {
-                        Id = 4,
                         ProductName = "Authentication and Security in ASP.NET",
                         ProductDescription = "Understand the implementation of authentication and authorization, protect sensitive data, and prevent common vulnerabilities like SQL injection and XSS in your web applications.",
                         Image = "NET4.png",
@@ -77,7 +73,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new Product
                     {
-                        Id = 5,
                         ProductName = "Data Access with Entity Framework",
                         ProductDescription = "Master data operations using Entity Framework, execute CRUD operations, and query data efficiently using LINQ within your ASP.NET applications.",
                         Image = "NET5.png",
@@ -85,7 +80,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new Product
                     {
-                        Id = 6,
                         ProductName = "Mastering ASP.NET MVC",
                         ProductDescription = "Delve into the principles of the MVC architecture, create robust models, design interactive views, and handle complex controller logic, streamlining your web application development.",
                         Image = "NET6.png",
@@ -93,7 +87,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new Product
                     {
-                        Id = 7,
                         ProductName = "Web APIs and RESTful Services in ASP.NETC",
                         ProductDescription = "Design and develop RESTful services using ASP.NET Web API. Understand the intricacies of HTTP, and create a solid backend for cross-platform applications.",
                         Image = "NET7.png",
@@ -101,7 +94,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new Product
                     {
-                        Id = 8,
                         ProductName = "Middleware and Dependency Injection in ASP.NET Core",
                         ProductDescription = "Configure and utilize middleware and understand the power of dependency injection to write modular, maintainable, and testable code in ASP.NET Core applications.",
                         Image = "NET8.png",
@@ -109,7 +101,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new Product
                     {
-                        Id = 9,
                         ProductName = "Unit and Integration Testing in ASP.NET",
                         ProductDescription = "Implement reliable unit and integration testing practices, use mock objects for testing, ensuring your ASP.NET application is robust and error-free before deployment.",
                         Image = "NET9.png",
@@ -117,7 +108,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new Product
                     {
-                        Id = 10,
                         ProductName = "Deploying ASP.NET Applications",
                         ProductDescription = "Master the deployment of ASP.NET applications in various environments, including configuring IIS, and containerizing applications with Docker for real-world production scenarios.",
                         Image = "NET10.png",
@@ -125,7 +115,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new Product
                     {
-                        Id = 11,
                         ProductName = "Advanced Topics in ASP.NET Core",
                         ProductDescription = "Explore advanced features and capabilities of ASP.NET Core, diving deep into areas such as global exception handling, advanced middleware, custom filters, and performance optimization. Ideal for developers who want to leverage ASP.NET Core to its fullest potential.",
                         Image = "NET11.png",
@@ -133,7 +122,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new Product
                     {
-                        Id = 12,
                         ProductName = "Building Real-Time Applications with SignalR",
                         ProductDescription = "Learn to develop real-time web applications using ASP.NET Core SignalR. This course covers the concepts of real-time data updates, live communication, and seamless client-server messaging, essential for applications like live chats, instant notifications, and collaborative platforms.",
                         Image = "NET12.png",
@@ -143,19 +131,16 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                 context.Cart.AddRange(
                     new Cart
                     {
-                        Id = 1,
                         UserId = "c7b4d3b5-e0ec-49e3-8d5f-97d6a42e7be7"
                     },
                     new Cart
                     {
-                        Id = 2,
                         UserId = "4f6d6864-ec3b-4519-ad32-28f7a5f4364d"
                     }
                 );
                 context.CartDetail.AddRange(
                     new CartDetail
                     {
-                        Id = 1,
                         CartId = 1,
                         ProductId = 12,
                         Quantity = 1,
@@ -165,28 +150,24 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                 context.Order.AddRange(
                     new Order
                     {
-                        Id = 1,
                         UserId = "c7b4d3b5-e0ec-49e3-8d5f-97d6a42e7be7",
                         CreateDate = DateTime.Parse("2021-05-20 12:35:27.6614047"),
                         IsDeleted = false,
                     },
                     new Order
                     {
-                        Id = 2,
                         UserId = "c7b4d3b5-e0ec-49e3-8d5f-97d6a42e7be7",
                         CreateDate = DateTime.Parse("2021-09-16 13:09:39.9067394"),
                         IsDeleted = false,
                     },
                     new Order
                     {
-                        Id = 3,
                         UserId = "c7b4d3b5-e0ec-49e3-8d5f-97d6a42e7be7",
                         CreateDate = DateTime.Parse("2022-08-31 13:29:07.9874774"),
                         IsDeleted = false,
                     },
                     new Order
                     {
-                        Id = 4,
                         UserId = "c7b4d3b5-e0ec-49e3-8d5f-97d6a42e7be7",
                         CreateDate = DateTime.Parse("2023-10-25 15:32:01.2197073"),
                         IsDeleted = false,
@@ -195,7 +176,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                 context.OrderDetail.AddRange(
                     new OrderDetail
                     {
-                        Id = 1,
                         OrderId = 1,
                         ProductId = 2,
                         UnitPrice = 159.99,
@@ -203,7 +183,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 2,
                         OrderId = 1,
                         ProductId = 2,
                         UnitPrice = 159.99,
@@ -211,7 +190,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 3,
                         OrderId = 1,
                         ProductId = 2,
                         UnitPrice = 159.99,
@@ -219,7 +197,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 4,
                         OrderId = 1,
                         ProductId = 7,
                         UnitPrice = 64.99,
@@ -227,7 +204,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 5,
                         OrderId = 1,
                         ProductId = 7,
                         UnitPrice = 64.99,
@@ -235,7 +211,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 6,
                         OrderId = 1,
                         ProductId = 7,
                         UnitPrice = 64.99,
@@ -243,7 +218,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 7,
                         OrderId = 2,
                         ProductId = 2,
                         UnitPrice = 159.99,
@@ -251,7 +225,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 8,
                         OrderId = 2,
                         ProductId = 2,
                         UnitPrice = 159.99,
@@ -259,7 +232,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 9,
                         OrderId = 2,
                         ProductId = 2,
                         UnitPrice = 159.99,
@@ -267,7 +239,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 10,
                         OrderId = 2,
                         ProductId = 2,
                         UnitPrice = 159.99,
@@ -275,7 +246,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 11,
                         OrderId = 2,
                         ProductId = 2,
                         UnitPrice = 159.99,
@@ -283,7 +253,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 12,
                         OrderId = 2,
                         ProductId = 5,
                         UnitPrice = 69.49,
@@ -291,7 +260,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 13,
                         OrderId = 2,
                         ProductId = 5,
                         UnitPrice = 69.49,
@@ -299,7 +267,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 14,
                         OrderId = 2,
                         ProductId = 5,
                         UnitPrice = 69.49,
@@ -307,7 +274,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 15,
                         OrderId = 2,
                         ProductId = 1,
                         UnitPrice = 648.99,
@@ -315,7 +281,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 16,
                         OrderId = 3,
                         ProductId = 2,
                         UnitPrice = 159.99,
@@ -323,7 +288,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 17,
                         OrderId = 3,
                         ProductId = 2,
                         UnitPrice = 159.99,
@@ -331,7 +295,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 18,
                         OrderId = 3,
                         ProductId = 2,
                         UnitPrice = 159.99,
@@ -339,7 +302,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 19,
                         OrderId = 4,
                         ProductId = 3,
                         UnitPrice = 199.99,
@@ -347,7 +309,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 20,
                         OrderId = 4,
                         ProductId = 2,
                         UnitPrice = 159.99,
@@ -355,7 +316,6 @@ namespace Shopping_Cart_Web_Application_V1._0.Models
                     },
                     new OrderDetail
                     {
-                        Id = 21,
                         OrderId = 4,
                         ProductId = 1,
                         UnitPrice = 648.99,
